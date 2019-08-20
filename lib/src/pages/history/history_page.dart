c8dbbae300c567083974872878129e026e535bbd
Ivan Martinez <ivanmart9503>
2019-08-20 09:18:29 -0500

    Cambios menores.

--- lib/src/pages/history/history_page.dart
+++ lib/src/pages/history/history_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:plantilin/src/stores/main_store.dart';
 import 'package:provider/provider.dart';
 
+// Pagina del historial
 class HistoryPage extends StatelessWidget {
+  // Funcion principal que pinta la pagina en la pantalla
   @override
   Widget build(BuildContext context) {
     final store = Provider.of<MainStore>(context);
@@ -13,14 +15,23 @@ class HistoryPage extends StatelessWidget {
         color: Colors.white,
         child: Observer(
           builder: (_) => store.isConnected
+              // Contenedor principal del tamaño de la pantalla
               ? Container(
+                  // Una lista con separadores
                   child: ListView.separated(
+                    // El divider es la linea separadora
                     separatorBuilder: (context, index) => Divider(
                       color: Colors.grey,
                     ),
+                    // El itemCount se le pasa cuantos elementos pintar en pantalla
                     itemCount: store.data.length,
+                    // Aqui se crea el elemento que se va a repetir
+                    // el numero de veces que el itemCount tiene
                     itemBuilder: (context, int index) {
                       Map<String, dynamic> data = store.data[index];
+                      // Con el list tile se pinta si es temperatura, humedad, etc.
+                      // tambien el dato por ejemplo: 20 grados, 50 grados, 30 por ciento
+                      // y al final la fecha de cuando se recibio ese dato
                       return ListTile(
                         title: Text(
                           data['type'],
@@ -44,6 +55,8 @@ class HistoryPage extends StatelessWidget {
                     },
                   ),
                 )
+              // Si no esta conectado a un servidor mqtt no puede ver el historial
+              // y se muestra un texto que dice eso
               : Center(
                   child: Text(
                     'Conéctate para ver el historial',
